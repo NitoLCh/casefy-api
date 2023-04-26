4bd3705a49a333438806467860fd869a5286b442
nitolch <jorgealejandrolch2@gmail.com>
2023-04-26 17:57:49 -0600

    Creation of the endpoints. Connection with database Done. All users endpoints has been created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import app from './app.js';
-import {PORT} from './config.js';
+import {PORT, DB_HOST, DB_PORT} from './config.js';
 
 app.listen(PORT);
 console.log('listen on port ', PORT);
+console.log('listen on DB_PORT ', DB_PORT);
+console.log('listen on HOST ', DB_HOST);

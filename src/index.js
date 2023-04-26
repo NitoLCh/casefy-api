d7209f51846c8dfc239743de09c4ebd8acfc1797
nitolch <jorgealejandrolch2@gmail.com>
2023-04-26 08:52:34 -0600

    Creation of the endpoints. Connection with database Done. All users endpoints has been created

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import { pool } from './db/db.js';
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
 app.use(morgan('combined'));
 
 app.get('/ping', async (req, res) => {
@@ -15,12 +17,29 @@ app.get('/ping', async (req, res) => {
     res.json(result[0]);
 });
 
-app.get('/usuarios', (req, res) => res.send('obteniendo usuarios'));
+app.get(`/usuarios`, async (req, res) =>{ 
+    res.send('obteniendo usuarios');
+    const [result] = await pool.query('SELECT * FROM usuarios WHERE id = ?',[id])
+    res.json(result[0]);
+});
 
-app.post('/usuarios', (req, res) => res.send('creando usuarios'));
+app.post('/usuarios', async(req, res) => {
+    res.send('creando usuarios');
+    const [result] = await pool.query('INSERT INTO usuarios SET ?',[req.body])
+    res.json(result[0]);
+});
 
-app.put('/usuarios', (req, res) => res.send('actualizando usuarios'));
+app.put('/usuarios', async (req, res) => {
+    res.send('actualizando usuarios');
+    const [result] = await pool.query('UPDATE usuarios SET ? WHERE id = ?',[req.body, id]);
+    res.json(result[0]);
+});
 
-app.delete('/usuarios', (req, res) => res.send('eliminando usuarios'));
+app.delete('/usuarios', async (req, res) => {
+    res.send('eliminando usuarios');
+    const [result] = await pool.query('DELETE FROM usuarios WHERE id = ?',[id]);
+    res.json(result[0]);
+});
+
 
 app.listen(3001, () => console.log('listen in port 3001'));

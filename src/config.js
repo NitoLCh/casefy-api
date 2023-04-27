b929694c9c528a6583b6dc3215964cb545113f61
nitolch <jorgealejandrolch2@gmail.com>
2023-04-26 18:24:58 -0600

    Creation of the endpoints. Connection with database Done. All users endpoints has been created

--- src/config.js
+++ src/config.js
@@ -8,4 +8,4 @@ export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASS = process.env.DB_PASS || 'mateakeni20';
-export const DB_DATABASE = process.env.DB_NAME || 'bdcasefy';
+export const DB_DATABASE = process.env.DB_DATABASE || 'bdcasefy';

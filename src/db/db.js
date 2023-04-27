da9a423b0eb85eb26229f8b87e6371835ced9b38
nitolch <jorgealejandrolch2@gmail.com>
2023-04-26 18:19:04 -0600

    Creation of the endpoints. Connection with database Done. All users endpoints has been created

--- src/db/db.js
+++ src/db/db.js
@@ -2,9 +2,9 @@ import {createPool} from 'mysql2/promise';
 import {DB_HOST, DB_USER, DB_PASS, DB_DATABASE, DB_PORT} from '../config.js';
 
 export const pool = createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    port: DB_PORT,
-    database: DB_DATABASE,
+    host: "containers-us-west-80.railway.app",
+    user: "root",
+    password: "WA1O5W6Z9DVD2SsL2Qpd",
+    port: 6251,
+    database: "railway",
 });
